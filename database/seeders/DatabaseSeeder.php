5a95cf8b21d1630ec7a8bafb9237e19bf990e9de
Geoff Jukes <george.jukes@gmail.com>
2023-09-20 21:51:49 +0100

    Add a set of books to the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,42 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $data = [
+            'J.R.R. Tolkien' => [
+                [
+                    'title' => 'The Fellowship of the Ring',
+                    'genre' => 'Fantasy',
+                    'release_date' => '1954-07-29',
+                ],
+                [
+                    'title' => 'The Two Towers',
+                    'genre' => 'Fantasy',
+                    'release_date' => '1954-11-11',
+                ],
+                [
+                    'title' => 'The Return of the King',
+                    'genre' => 'Fantasy',
+                    'release_date' => '1955-10-20',
+                ],
+            ],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($data as $authorName => $books) {
+            $author = Author::create([
+                'name' => $authorName,
+            ]);
+
+            foreach ($books as $book) {
+                $author->books()->create($book);
+            }
+        }
     }
 }
