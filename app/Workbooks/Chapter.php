443813bc1e4a5fbfafbc242821f07685689d6f6d
Geoff10 <george.jukes@googlemail.com>
2024-11-20 23:15:47 +0000

    Add getContent to chapter
    This automatically adds the preview route to code blocks.

--- app/Workbooks/Chapter.php
+++ app/Workbooks/Chapter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Workbooks;
 
+use Illuminate\Support\Str;
+
 abstract class Chapter
 {
     abstract public function id(): string;
@@ -15,11 +17,35 @@ abstract class Chapter
         $array = [
             'id' => $this->id(),
             'title' => $this->title(),
-            'content' => $this->content(),
+            'content' => $this->getContent(),
         ];
 
         return $properties ?
             array_filter($array, fn ($key) => in_array($key, $properties), ARRAY_FILTER_USE_KEY) :
             $array;
+    }
+
+    public function workbookId(): string
+    {
+        return Str::of($this::class)
+            ->replaceFirst('App\\Workbooks\\', '')
+            ->explode('\\')
+            ->first();
+    }
+
+    public function getContent(): array
+    {
+        return collect($this->content())
+            ->map(function (array $content, int $index) {
+                if ($content['type'] === 'runnableCodeBlock' && !isset($content['route'])) {
+                    $content['route'] = route('preview', [
+                        'workbook' => $this->workbookId(),
+                        'chapter' => $this->id(),
+                        'exercise' => $index,
+                    ]);
+                }
+
+                return $content;
+            })->toArray();
     }
 }
