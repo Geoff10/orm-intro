592931ac84bfce7f9f5f0d7a2e8804bf385803de
Geoff <george.jukes@googlemail.com>
2025-03-10 00:41:07 +0000

    Add event for broadcasting job updates

---
+++ app/Events/JobStatusChanged.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class JobStatusChanged implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public string $uniqueSessionId,
+        public string $jobId,
+        public string $status,
+    ) {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('session.' . $this->uniqueSessionId),
+        ];
+    }
+
+    /**
+    * The event's broadcast name.
+    */
+    public function broadcastAs(): string
+    {
+        return 'JobStatusChanged';
+    }
+}
